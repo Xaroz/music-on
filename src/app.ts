a64dbdcdce08f9ad3a8453eeffe53695592461d3
Bant89 <bant8009@gmail.com>
2023-06-20 18:37:53 -0400

    added global error handler

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,9 @@ import cookieParser from 'cookie-parser';
 import trackRouter from './routes/trackRoutes';
 import userRouter from './routes/userRoutes';
 
+import AppError from './utils/appError'
+import globalErrorHandler from './controllers/errorController'
+
 // Start express app
 const app = express();
 
@@ -20,5 +23,11 @@ app.use(cookieParser());
 // Routes
 app.use('/api/v1/tracks', trackRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+})
+
+app.use(globalErrorHandler)
 
 export default app;
