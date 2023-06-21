0d74e79564939f43a97449984469a88d65183bb8
Bant89 <bant8009@gmail.com>
2023-06-21 07:29:52 -0400

    fixed typo

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,9 +1,8 @@
-export const DAY_IN_MILISECONDS = 24 * 60 * 60 * 1000;
+export const DAY_IN_MILLISECONDS = 24 * 60 * 60 * 1000;
 
 export enum RuntimeEnv {
-  PRODUCTION = "production",
-  DEVELOPMENT = "development",
+  PRODUCTION = 'production',
+  DEVELOPMENT = 'development',
 }
 
-export const JWT_COOKIE = "jwt";
-
+export const JWT_COOKIE = 'jwt';
